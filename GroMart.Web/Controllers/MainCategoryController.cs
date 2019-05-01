e006b25b7733328396410a675f061040bb32841d
Mubeen4545 <mubeenahmad860@gmail.com>
2019-05-01 06:28:30 -0700

    Added Category Product Make Business Entities and Created Database

---
+++ GroMart.Web/Controllers/MainCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using GroMart.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroMart.Web.Controllers
+{
+    public class MainCategoryController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(MainCategory maincategory)
+        {
+            return View();
+        }
+    }
+}
